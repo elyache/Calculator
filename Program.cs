ce4554332cfd1abebd926825d04aab1d533082cf
elyache <rittori2003@gmail.com>
2025-01-13 20:58:50 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+        }
+    }
+
+    public class Calculator
+    {
+        int Num1 { get; set; }
+        int Num2 { get; set; }
+
+        public void Opertion()
+        {
+
+        }
+    }
+
+    public class Multiply : Calculator
+    {
+
+    }
+}
